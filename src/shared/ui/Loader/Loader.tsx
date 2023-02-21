700601fa85753961997fca4623dd243da9915194
amangeldi0 <amanimanbekov12@gmail.com>
2023-02-21 23:25:29 +0600

    errorBoundary loader notFoundpage

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,24 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './Loader.module.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({ className } : LoaderProps) => (
+    <div className={classNames(cls.ldsSpinner, {}, [className])}>
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+        <div className={cls.div} />
+    </div>
+);
