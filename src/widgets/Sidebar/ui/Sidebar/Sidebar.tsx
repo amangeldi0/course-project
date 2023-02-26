b212b144b6542252c5949a0d6fb933bb66f95c7d
amangeldi0 <amanimanbekov12@gmail.com>
2023-02-26 10:11:14 +0600

    fix lint errors

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState<boolean>(true);
+    const { t } = useTranslation();
 
     return (
         <div
@@ -21,13 +23,13 @@ export const Sidebar = ({ className }: SidebarProps) => {
             )}
             data-testid="sidebar"
         >
-            {/* <button */}
-            {/*    type="button" */}
-            {/*    data-testid="sidebar-toggle" */}
-            {/*    onClick={() => setCollapsed((prevState) => !prevState)} */}
-            {/* > */}
-            {/*    toogle */}
-            {/* </button> */}
+            <button
+                type="button"
+                data-testid="sidebar-toggle"
+                onClick={() => setCollapsed((prevState) => !prevState)}
+            >
+                {t('toggle')}
+            </button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher />
